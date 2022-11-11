1fb9bfbcd44645371afab7eb59553b0b1c42b1f7
ELJ <47494925+EllieJaybee@users.noreply.github.com>
2022-11-11 12:04:16 +0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,218 @@
+const femDict = {
+  amelie: {
+    casedName: "Amelie",
+    pfp: "assets/img/amelie.jpg",
+    colors: "red, yellow, purple, navy",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/PrettyBoyAmelie"
+      }
+    ]
+  },
+  diamy: {
+    casedName: "Diamy",
+    pfp: "assets/img/diamy.jpg",
+    colors: "blue, lightpink, pink, purple",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/AugustCutieTTV"
+      },{
+      //  class: "twc",
+      //  link: "https://twitch.tv/diamycutieangel"
+      //},{
+        class: "stm",
+        link: "https://steamcommunity.com/id/Diamy_VR"
+      }
+    ]
+  },
+  druzie: {
+    casedName: "Druzie",
+    pfp: "assets/img/suicideboy1.png",
+    colors: "#000000, #97095f, #c41b71, #ffffff",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/DruzieTwt"
+      },{
+        class: "ins",
+        link: "https://www.instagram.com/druzie_"
+      },{
+        class: "fbk",
+        link: "https://www.facebook.com/xavion.lch"
+      },{
+        class: "stm",
+        link: "https://steamcommunity.com/id/DruzieSteam"
+      }
+    ]
+  },
+  elliejb: {
+    casedName: "EllieJB",
+    pfp: "assets/img/catboy.png",
+    colors: "#642CA9, #FF36AB, #FF74D4, #FFB8DE",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/magaysian"
+      },{
+        class: "ptr",
+        link: "https://patreon.com/EllieJB"
+      },{
+        class: "dsc",
+        link: "https://discord.femboy.my"
+      },{
+        class: "stm",
+        link: "https://steamcommunity.com/id/elliejb/"
+      }
+    ]
+  },
+  jun: {
+    casedName: "Jun",
+    pfp: "https://media.discordapp.net/attachments/1029117539411837018/1031264934551621652/unknown-16.png",
+    colors: "pink, purple",
+    socials: [
+      {
+        class: "stm",
+        link: "https://steamcommunity.com/id/reenox03/"
+      }
+    ]
+  },
+  kokorofujiwara: {
+    casedName: "Kokoro Fujiwara",
+    pfp: "https://kokorofujiwara.carrd.co/assets/images/image01.jpg",
+    colors: "#c471b0, #d17ba9, #b9707e, #c6806e",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/FujiwaraKokoro"
+      },{
+        class: "ytb",
+        link: "https://www.youtube.com/channel/UC9re3kaLKFZ8HpxPpidj21A"
+      },{
+        class: "twc",
+        link: "https://www.twitch.tv/kokoro_ch4n"
+      }
+    ]
+  },
+  koekoreaper: {
+    casedName: "KoekoReaper",
+    pfp: "assets/img/koekoshock.webp",
+    colors: "#ff4181, #b21549, #800000, #370000",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/KoekoReaper"
+      },{
+        class: "ins",
+        link: "https://instagram.com/KoekoReaper"
+      },{
+        class: "kfi",
+        link: "https://ko-fi.com/koekoreaper"
+      }
+    ]
+  },
+  lulu: {
+    casedName: "Lulu",
+    pfp: "https://cdn.discordapp.com/attachments/986540985037246474/1006977099447422976/VRChat_1920x1080_2022-03-23_10-28-41.896_2.png",
+    colors: "#fbb3ed, #ea41c9, #fa9083, #5ef9e1",
+    socials: [
+      {
+      //  class: "ins",
+      //  link: "https://instagram.com/luludyinginside"
+      //},{
+        class: "stm",
+        link: "https://steamcommunity.com/profiles/76561199118130672/"
+      }
+    ]
+  },
+  lumienze: {
+    casedName: "Lumienze",
+    pfp: "https://cdn.discordapp.com/avatars/357402169848365056/745aa1b47668a6077ee0db84ba30515d.png",
+    colors: "white, red, black, white",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/lumienzeowl"
+      }
+    ]
+  },
+  luzeira: {
+    casedName: "Luzeira",
+    pfp: "https://media.discordapp.net/attachments/1037758544340058132/1040339633470451804/received_438063736996032.png",
+    colors: "#55d4cd, #e876d5",
+    socials: [
+      {
+        class: "fbk",
+        link: "https://www.facebook.com/Dragiblast"
+      },{
+        class: "ins",
+        link: "https://www.instagram.com/luualter"
+      }
+    ]
+  },
+  onishifumiko: {
+    casedName: "OnishiFumiko",
+    pfp: "assets/img/fumi.jpg",
+    colors: "white, #ffb7c5, magenta, black",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/OnishiFumikoVT"
+      },{
+        class: "ytb",
+        link: "https://www.youtube.com/channel/UCEVRBllOjpHYjND4XTU7ISA"
+      },{
+        class: "twc",
+        link: "https://www.twitch.tv/onishifumiko"
+      },{
+        class: "kfi",
+        link: "https://ganknow.com/OnishiFumiko"
+      }
+    ]
+  },
+  phillette:{
+    casedName: "Phillette",
+    pfp: "assets/img/peel.png",
+    colors: "#e8a2e8, white",
+    socials: [
+      {
+        class: "twt",
+        link: "https://twitter.com/PhilletteP"
+      }
+    ]
+  },
+  vinnie: {
+    casedName: "Vinnie",
+    pfp: "assets/img/vinnie.png",
+    colors: "white, yellow, orange, red",
+    socials: [
+      {
+        class: "ins",
+        link: "https://www.instagram.com/maou_vinnie"
+      },{
+        class: "stm",
+        link: "https://steamcommunity.com/id/MaouVinnie/"
+      },{
+        class: "twt",
+        link: "https://twitter.com/MaouVinnie"
+      }
+    ]
+  },
+  '6err0r9': {
+    casedName: "6Err0r9",
+    pfp: "assets/img/error.png",
+    colors: "#595e82, #cfbdc5, #333457, #080412",
+    socials: [
+      {
+        class: "ytb",
+        link: "https://www.youtube.com/channel/UCdnyiUwz1ghsHVnQsMknfyQ"
+      },{
+        class: "twc",
+        link: "https://www.twitch.tv/lleafy__"
+      }
+    ]
+  }
+}
+
+export default femDict;
